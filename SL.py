67d42dedd9b651e8e5fe0b2827b0805023aefbb0
Paul Dawidowicz <nkipa2057@gmail.com>
2024-04-02 21:23:20 +0700

    Update SL.py

--- SL.py
+++ SL.py
@@ -6,7 +6,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-df = pd.read_csv('MVP_NUTRITION_4_1_2024.csv',on_bad_lines='skip',low_memory=False)
+df = pd.read_csv('MVP_NUTRITION_4_2_2024.csv',on_bad_lines='skip',low_memory=False)
 
 df.index = range(1,len(df)+1)
 st.set_page_config(
@@ -16,18 +16,11 @@ st.set_page_config(
    initial_sidebar_state="expanded",
 )
 
-
 st.title("NutrAGI - Nutritional Assistant")
 st.subheader ("Meal Suggestion and Traking App")
-#x = st.number_input ("Food Number [1-1500]",max_value=1500, min_value=0)
-
 
 col1,col2,col3,col4=st.columns(4)
 col11,col12,col13,col14=st.columns(4)
-
-
-
-#st.write(f'Food {x}')
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -36,13 +29,6 @@ df.columns = df.columns.str.replace('_', ' ')
 df.rename(columns={"R": "Restaurant", "M": "Supermarket", "H": "Home", "P": "Preferred", "I": "Indifferent", "K": "Avoid", "Q":"Quantity","C": "Cost"},inplace=True)
 df.columns=df.columns.str.capitalize()
 
-
-#dfq=df.loc[(df["Name"] == f"Food {x}")]
-
-#sol=dfq.loc[:, ~(dfq == 0).all()].T
-#sol['Total'] = sol.iloc[2::].sum(axis = 1)
-#sol['Total'] = sol['Total'].fillna('')
-#st.write(sol)
 
 #st.line_chart(sol)
 with col1:
@@ -61,6 +47,11 @@ with col2:
         "Location of the meal",
         ['Restaurant', 'Supermarket','Home','Other']
 )
+    R = st.checkbox('Restaurant')
+    M = st.checkbox('Super Market')
+    H = st.checkbox('Home')
+    O = st.checkbox('Other')
+
 with col12:    
     st.write("Your location is:", location)
 with col3:
@@ -76,16 +67,13 @@ dfsol=df[(df[meal]==1)&(df[location]==1)&(df[preference]==1)]
 with st.expander("Food List available for your choise"):
     st.dataframe(dfsol)
 
-list=list(dfsol.Name)
-#dfsol
+today_nutr=list(dfsol.Name)
+#today_nutr
 with col4:
     y = st.multiselect (
         "Food Selector (Select Food Number from dropDown Menu)",
-        list                   
+        today_nutr                   
     )
-
-#st.write('You selected:', y)
-#df1=dfsol.loc[(dfsol["Name"] == y)]
 
     sol=dfsol[dfsol["Name"].isin(y)].T
 cola,col1b=st.columns(2)
@@ -104,18 +92,26 @@ with cola:
                   'Alcohol', 'Ash', 'Water']]
     with st.expander("Food Consumed"):
             st.dataframe(sol)
-#  
-#sol.Total
-#sol.Total = pd.to_numeric(sol.Total, errors='coerce')
-#sol.Total = sol.Total.map('{:.3f}'.format)
-#st.dataframe(data=None, width=None, height=None, use_container_width=True, hide_index=None, column_order=None, column_config=None)
-#sol
+total_cost=format(sol.Total.Cost,'.2f')
+cola1,colb1,colc1,cold1, cole1,colf1=st.columns(6)
+with col1b:
+    with cola1:
+        st.metric(label="Cost ($)", value=format(sol.Total.Cost,'.2f'))
+    with colb1:
+        st.metric(label="Protein (gr)", value=format(sol.Total.Protein,'.2f'))
+    with colc1:
+        st.metric(label="Calories (gr.)", value=format(sol.Total.Calories,'.2f'))
+    with cold1:
+        st.metric(label="Carbohydrate (gr.)", value=format(sol.Total.Carbohydrate,'.2f'))
+    with cole1:
+        st.metric(label="Fat (gr.)", value=format(sol.Total.Fat,'.2f'))
+    with colf1:
+        st.metric(label="Fiber (gr.)", value=format(sol.Total.Fiber,'.2f'))
+    with cola1:
+        st.metric(label="Cholesterol (mg.)", value=format(sol.Total.Cholesterol,'.2f'))
 
-#st.write(f'Food {y}')
 
-#dfq=df.loc[(df["Name"] == f"Food {x}")]
 
-#sol=dfq.loc[:, ~(dfq == 0).all()].T
-#sol['Total'] = sol.iloc[2::].sum(axis = 1)
-#sol['Total'] = sol['Total'].fillna('')
-#st.write(sol)
+
+
+     
